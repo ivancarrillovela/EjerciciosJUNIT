d77b5ee62ad3d77ecf49688fa598f1b4138f7df0
Ivan <ivancarrillovela@gmail.com>
2025-04-22 19:45:51 +0200

    Terminado el metodo de la clase Alumno

--- src/main/java/org/cuatrovientos/dam/ed/EjercicioTipoExamen/Alumno.java
+++ src/main/java/org/cuatrovientos/dam/ed/EjercicioTipoExamen/Alumno.java
@@ -1,5 +1,29 @@
 package org.cuatrovientos.dam.ed.EjercicioTipoExamen;
 
+import java.util.ArrayList;
+
 public class Alumno {
+	
+	private String nombre;
+	private String email;
+	private ArrayList<Modulo> modulos=new ArrayList<Modulo>();
+	
+	public Alumno(String nombre, String email) {
+		super();
+		this.nombre = nombre;
+		this.email = email;
+	}
+
+	public boolean apruebaCurso() {
+		if (modulos.isEmpty()) {
+			return true;
+		}
+		for (Modulo m:modulos) {
+			if (m.notaModulo()<5) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
