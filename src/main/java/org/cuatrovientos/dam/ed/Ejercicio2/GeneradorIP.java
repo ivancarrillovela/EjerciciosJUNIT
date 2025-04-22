632778d942ae0880a6521f94a25f0d3dc0837983
Ivan <ivancarrillovela@gmail.com>
2025-04-22 17:13:41 +0200

    Creados los metodos de la clase GeneradorIP

--- src/main/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIP.java
+++ src/main/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIP.java
@@ -1,5 +1,22 @@
 package org.cuatrovientos.dam.ed.Ejercicio2;
 
+import java.util.Random;
+
 public class GeneradorIP {
 	
+	private Random random=new Random();
+	
+	public int generarNumero(){
+		return random.nextInt(0,255);
+	}
+	
+	public String generarIPV4(){
+		String miIp = "";
+		for (int i=0;i<4;i++) {
+			if (generarNumero()!=0) {
+				miIp+=generarNumero()+".";
+			}
+		}
+		return miIp;
+	}
 }
