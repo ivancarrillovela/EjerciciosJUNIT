e4368569c01d648766cccf55774e95c2a967047a
Ivan <ivancarrillovela@gmail.com>
2025-04-22 18:34:51 +0200

    Corregidos errores del metodo generarIPV4 y terminado test del mismo

--- src/main/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIP.java
+++ src/main/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIP.java
@@ -7,15 +7,23 @@ public class GeneradorIP {
 	private Random random=new Random();
 	
 	public int generarNumero(int min, int max){
-		return random.nextInt(min,max+1);
+		return random.nextInt(min,(max+1));
 	}
 	
 	public String generarIPV4(){
+		int[] secciones=new int[4];
 		String miIp = "";
-		for (int i=0;i<4;i++) {
-			if (generarNumero()!=0) {
-				miIp+=generarNumero()+".";
+		for (int i=0;i<secciones.length;i++) {
+			if(i==0 || i==secciones.length-1) {
+					secciones[i]=generarNumero(1, 254);
+			}else {
+				secciones[i]=generarNumero(0, 254);
 			}
+			
+			if (i>0) {
+				miIp+=".";
+			}
+			miIp+=secciones[i];
 		}
 		return miIp;
 	}
--- src/test/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIPTest.java
+++ src/test/java/org/cuatrovientos/dam/ed/Ejercicio2/GeneradorIPTest.java
@@ -11,16 +11,24 @@ class GeneradorIPTest {
 		GeneradorIP generadorIP=new GeneradorIP();
 		
 		for (int i=0;i<1000;i++) {
-			int numero=generadorIP.generarNumero(0, 255);
-			assertTrue(numero <=255 && numero >=0, "El número está fuera del rango introducido");
+			int resultado=generadorIP.generarNumero(0, 255);
+			
+			assertTrue(resultado <=255 && resultado >=0, "El número está fuera del rango introducido");
 		}
 		
-	
 	}
 
 	@Test
 	void testGenerarIPV4() {
-		fail("Not yet implemented");
+		GeneradorIP generadorIP=new GeneradorIP();
+		
+		String resultado=generadorIP.generarIPV4();
+		
+		String[] secciones=resultado.split("\\.");
+		
+		assertTrue(secciones.length==4,"ERROR, la IPV4 no tiene el número de secciones indicado");
+		assertTrue(Integer.parseInt(secciones[0]) !=0 && Integer.parseInt(secciones[secciones.length-1]) !=0, "ERROR, la IPV4 empieza o termina por 0");
+		
 	}
 
 }
